3176fb1a555269a930afdb2ef64ed8808bd94c71
aariveros <andres.a.riveros@gmail.com>
2014-07-09 12:22:33 -0400

    Cambio sampling a folds y arreglo index

--- sampling.py
+++ sampling.py
@@ -6,15 +6,14 @@ from sklearn import cross_validation
 
 from config import *
 
-def sampling_data(path, percent=10):
+def sampling_data(path, fold=10):
   data = pd.read_csv(path)
 
-  folds = math.trunc(100/percent)
-  skf = cross_validation.StratifiedKFold(data['class'], n_folds=folds)
+  skf = cross_validation.StratifiedKFold(data['class'], n_folds=fold)
 
   for train_index, test_index in skf:
     data = data.iloc[test_index]
     break
 
   name = path.split('/')[-1].split('.')[0]
-  data.to_csv(SETS_PATH + "/" + name + " sampled " + str(percent) + " percent.csv")
+  data.to_csv(SETS_PATH + "/" + name + " sampled " + str(percent) + " folds.csv", index=False)
