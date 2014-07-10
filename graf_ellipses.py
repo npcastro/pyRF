f99836b385b26b73af877c8828002a7f2deee95f
Nicolas Castro <npcastrol@gmail.com>
2014-07-10 17:10:12 -0400

    Junto distintos metodos para graficar en un solo metodo.

--- graf_ellipses.py
+++ graf_ellipses.py
@@ -4,35 +4,62 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.patches import Ellipse
 
-
-def graf_ellipses(path, clases, x, y, x_value, y_value):
-	data = pd.read_csv(path)
-
-	# Elijo solo algunas clases
-	criterion = data['class'].map(lambda x: x in clases)
-	data = data[criterion]
-
-	# obtengo los valores de las elipses
-	x_mean = data[x + '.mean'].tolist()
-	# x_margin = ((data[x + '.r'] - data[x + '.l'])/1).tolist()
-	x_margin = (data[x + '.std'] * 6).tolist()
-
-	y_mean = data[y + '.mean'].tolist()
-	# y_margin = ((data[y + '.r'] - data[y + '.l'])/1).tolist()
-	y_margin = (data[y + '.std'] * 6).tolist()
-
-	colors = { 'Be Stars': 'b', 'Cepheid': 'g', 'Eclipsing Binaries': 'r', 'Long Periodic Variable': 'c', 'MicroLensing': 'm', 'None Variable': 'y', 'Quasar': 'k', 'RR Lyrae':'w' }
-	class_list = data['class'].tolist()
-
+def draw_ellipses(ax, x_mean, y_mean, x_margin, y_margin, class_list, colors):
 	ells = []
 	for i in xrange(len(x_mean)):
 		ells.append( Ellipse(xy=[x_mean[i], y_mean[i]], width=x_margin[i], height=y_margin[i], angle=0, facecolor=colors[class_list[i]]) )
 
+	for e in ells:
+	    ax.add_artist(e)
+	    e.set_clip_box(ax.bbox)
+	    e.set_alpha(0.7)
+
+def draw_points():
+	pass
+
+def graf(path, clases, x, y, x_value, y_value, how='points', show=True):
+	""" Grafica datos con incertidumbre como elipses en el espacio.
+
+	Parameters
+	----------
+	path: string
+		El path al set de datos que se va a graficar.
+	clases: list(str)
+		Nombres de las clases que se van a considerar
+	x: string
+		nombre de la feature en el eje x
+	y: string
+		nombre de la feature en el eje y
+	x_value: float
+		valor donde se grafica una linea vertical
+	y_value: float
+		valor donde se grafica una linea horizontal
+	"""
+	data = pd.read_csv(path)
+
+	# Dejo solo las clases objetivo
+	criterion = data['class'].map(lambda x: x in clases)
+	data = data[criterion]
+
+	# Obtengo los valores de las elipses
+	x_mean = data[x + '.mean'].tolist()
+	x_margin = (data[x + '.std'] * 6).tolist()
+	y_mean = data[y + '.mean'].tolist()
+	y_margin = (data[y + '.std'] * 6).tolist()
+
+	# Colores para las distinas clases
+	colors = { 'Be Stars': 'b', 'Cepheid': 'g', 'Eclipsing Binaries': 'r', 'Long Periodic Variable': 'c', 'MicroLensing': 'm', 'None Variable': 'y', 'Quasar': 'k', 'RR Lyrae':'w' }
+	class_list = data['class'].tolist()
+
+	# Defino los margenes para el grafico
+	plt.xlim(min(x_mean) - max(x_margin), max(x_mean) + max(x_margin))
+	plt.ylim(min(y_mean) - max(y_margin), max(y_mean) + max(y_margin))
+
 	fig = plt.figure(1)
 	ax = fig.add_subplot(111)
 
-	plt.xlim(min(x_mean) - max(x_margin), max(x_mean) + max(x_margin))
-	plt.ylim(min(y_mean) - max(y_margin), max(y_mean) + max(y_margin))
+	if how == 'ellipses':
+		draw_ellipses(ax, x_mean, y_mean, x_margin, y_margin, class_list, colors)
 
 	plt.xlabel(x)
 	plt.ylabel(y, rotation = 'horizontal')
@@ -40,14 +67,13 @@ def graf_ellipses(path, clases, x, y, x_value, y_value):
 	plt.axhline(y = y_value, color = 'r')
 	plt.axvline(x = x_value, color = 'r')
 
-	for e in ells:
-	    ax.add_artist(e)
-	    e.set_clip_box(ax.bbox)
-	    e.set_alpha(0.7)
-
 	plt.title('utree ' + x + ' vs ' + y + '.png')
-	# plt.show()
-	plt.savefig('sets/utree ' + x + ' vs ' + y + '.png')
+
+	if show:
+		plt.show()
+	else:
+		plt.savefig('Resultados/utree ' + x + ' vs ' + y + '.png')
+
 	plt.close()
 
 def graf_points( path, clases, x, y, x_value, y_value ):
@@ -60,22 +86,21 @@ def graf_points( path, clases, x, y, x_value, y_value ):
 
 	colors = { 'Be Stars': 'b', 'Cepheid': 'g', 'Eclipsing Binaries': 'r', 'Long Periodic Variable': 'c', 'MicroLensing': 'm', 'None Variable': 'y', 'Quasar': 'k', 'RR Lyrae':'w' }
 
-	fig = plt.figure(1)
-	ax = fig.add_subplot(111)
-
+	# Encontrar margenes del grafico
 	x_mean = data[x + '.mean'].tolist()
-	# x_margin = ((data[x + '.r'] - data[x + '.l'])/1).tolist()
 	x_margin = (data[x + '.std'] * 6).tolist()
 	y_mean = data[y + '.mean'].tolist()
-	# y_margin = ((data[y + '.r'] - data[y + '.l'])/1).tolist()
 	y_margin = (data[y + '.std'] * 6).tolist()
 
 	plt.xlim(min(x_mean) - max(x_margin), max(x_mean) + max(x_margin))
 	plt.ylim(min(y_mean) - max(y_margin), max(y_mean) + max(y_margin))
 
+	fig = plt.figure(1)
+	ax = fig.add_subplot(111)
+
+	# Graficar
 	for c in clases:
 
-		# Valores a graficar
 		x_mean = data[data['class'] == c][x + '.mean'].tolist()
 		x_margin = ((data[x + '.r'] - data[x + '.l'])/1).tolist()
 		y_mean = data[data['class'] == c][y + '.mean'].tolist()
@@ -91,7 +116,7 @@ def graf_points( path, clases, x, y, x_value, y_value ):
 
 	plt.title('tree ' + x + ' vs ' + y + '.png')
 	# plt.show()
-	plt.savefig('sets/tree ' + x + ' vs ' + y + '.png')
+	plt.savefig('Resultados/tree ' + x + ' vs ' + y + '.png')
 	plt.close()
 
 def graf_points_and_ellipses( path, clases, x, y, x_value, y_value ):
@@ -104,9 +129,6 @@ def graf_points_and_ellipses( path, clases, x, y, x_value, y_value ):
 
 	colors = { 'Be Stars': 'b', 'Cepheid': 'g', 'Eclipsing Binaries': 'r', 'Long Periodic Variable': 'c', 'MicroLensing': 'm', 'None Variable': 'y', 'Quasar': 'k', 'RR Lyrae':'w' }
 
-	fig = plt.figure(1)
-	ax = fig.add_subplot(111)
-
 	x_mean = data[x + '.mean'].tolist()
 	x_margin = (data[x + '.std'] * 6).tolist()
 	y_mean = data[y + '.mean'].tolist()
@@ -115,17 +137,23 @@ def graf_points_and_ellipses( path, clases, x, y, x_value, y_value ):
 	plt.xlim(min(x_mean) - max(x_margin), max(x_mean) + max(x_margin))
 	plt.ylim(min(y_mean) - max(y_margin), max(y_mean) + max(y_margin))
 
-
 	class_list = data['class'].tolist()
 
+	fig = plt.figure(1)
+	ax = fig.add_subplot(111)
+
+	# Grafico elipses
 	ells = []
 	for i in xrange(len(x_mean)):
 		ells.append( Ellipse(xy=[x_mean[i], y_mean[i]], width=x_margin[i], height=y_margin[i], angle=0, facecolor=colors[class_list[i]]) )
 
+	for e in ells:
+	    ax.add_artist(e)
+	    e.set_clip_box(ax.bbox)
+	    e.set_alpha(0.7)	
 
+	# Grafico puntos
 	for c in ['None Variable', 'Long Periodic Variable']:
-
-		# Valores a graficar
 		x_mean = data[data['class'] == c][x + '.mean'].tolist()
 		x_margin = (data[x + '.std'] * 6).tolist()
 		y_mean = data[data['class'] == c][y + '.mean'].tolist()
@@ -133,26 +161,21 @@ def graf_points_and_ellipses( path, clases, x, y, x_value, y_value ):
 
 		ax.scatter( x_mean, y_mean, c = colors[c], alpha=0.7, zorder=100 )
 
-	for e in ells:
-	    ax.add_artist(e)
-	    e.set_clip_box(ax.bbox)
-	    e.set_alpha(0.7)	
-
 	plt.xlabel(x)
 	plt.ylabel(y, rotation = 'horizontal')
 
 	plt.axhline(y = y_value, color = 'r')
 	plt.axvline(x = x_value, color = 'r')
 
-	plt.title('Utree ' + x + ' vs ' + y + '.png')
+	plt.title('utree ' + x + ' vs ' + y + '.png')
 	# plt.show()
-	plt.savefig('sets/Utree ' + x + ' vs ' + y + '.png')
+	plt.savefig('Resultados/Utree ' + x + ' vs ' + y + '.png')
 	plt.close()
 
 if __name__ == '__main__':
-	# path = 'sets/macho random sampled 10 folds.csv'
+	path = 'sets/macho random sampled 10 folds.csv'
 	# path = 'sets/macho std variable.csv'
-	path = 'sets/macho artificial final.csv'
+	# path = 'sets/macho artificial final.csv'
 
 	# clases = [ 'Long Periodic Variable', 'MicroLensing', 'None Variable', 'RR Lyrae']
 	clases = [ 'Long Periodic Variable', 'None Variable', 'RR Lyrae']
@@ -170,7 +193,7 @@ if __name__ == '__main__':
 
 	# graf_points(path, clases, x, y, x_value, y_value)
 	# graf_points(path, clases, x, z, x_value, z_value)
-	graf_points(path, clases, y, z, y_value, z_value)
+	# graf_points(path, clases, y, z, y_value, z_value)
 
 	###### Cortes arbol incierto ######
 
@@ -178,10 +201,12 @@ if __name__ == '__main__':
 	y_value = 0.03
 	z_value = 0.04
 
-	# graf_ellipses(path, clases, x, y, x_value, y_value)
-	# graf_ellipses(path, clases, x, z, x_value, z_value)
-	# graf_ellipses(path, clases, y, z, y_value, z_value)
+	# graf(path, clases, x, y, x_value, y_value, how='ellipses')
+	# graf(path, clases, x, z, x_value, z_value, how='ellipses')
+	# graf(path, clases, y, z, y_value, z_value, how='ellipses')
 
-	graf_points_and_ellipses(path, clases, y, z, y_value, z_value)
+
+	###### Cortes inciertos y puntos ######
+	# graf_points_and_ellipses(path, clases, y, z, y_value, z_value)
 
 	
