ee79ea9fcd4c3cd4bc83f8aaf3f8c449115ec5a6
Nicolas Castro <npcastrol@gmail.com>
2014-07-14 12:07:07 -0400

    Agrego mas metodos reusables para graficar

--- graf.py
+++ graf.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 def draw_points( ax, df, x_name, y_name, colors):
 	"""Toma un dataframe y grafica dos variables como puntos con distintos colores
-	
+
 	ax: objeto axis de pyplot
 	df: dataframe con los datos a graficar. (Los nombres de las features deben venir con .mean)
 	x_name: nombre de la variable x
@@ -21,7 +21,17 @@ def draw_points( ax, df, x_name, y_name, colors):
 	for c in df['class'].unique().tolist():
 		x = df[df['class'] == c][x_name + aux].tolist()
 		y = df[df['class'] == c][y_name + aux].tolist()
-		ax.scatter( x, y, c = colors[c] )	
+		ax.scatter( x, y, c = colors[c] )
+
+def draw_splits( ax, x, y, color):
+	ax.axvline(x = x, color='r')
+	ax.axhline(y = y, color = 'r')
+
+def add_names( title, x_name, y_name):
+	plt.title(title)
+	plt.xlabel(x_name)
+	plt.ylabel(y_name, rotation = 'horizontal', labelpad=20)
+	
 
 if __name__ == '__main__':
 	path = 'sets/artificial random 10.csv'
@@ -33,6 +43,8 @@ if __name__ == '__main__':
 	ax = fig.add_subplot(111)
 
 	draw_points(ax, data, 'Feature 1', 'Feature 2', colors)
+	draw_splits(ax, 2.65, 1.95, 'r')
+	add_names('Feature 1 v/s 2', 'Feature 1', 'Feature 2')
 
 	plt.show()
 
