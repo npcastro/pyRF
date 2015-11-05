d92be3a786e44b8ec2b6931a1863e5771a25d9f4
Nicolas Castro <npcastrol@gmail.com>
2015-11-05 15:12:09 -0300

    Cambios a gitignore

--- f_score.py
+++ f_score.py
@@ -8,17 +8,25 @@ import pandas as pd
 clf = tree.Tree('gain', max_depth=10, min_samples_split=20)
 
 # result_dir = 'Resultados/Regular/Predicciones/'
-result_dir = 'Resultados/GP/Predicciones/'
+# result_dir = 'Resultados/GP/Predicciones/'
+# result_dir = '../Resultados/EROS/UTree/GP/Predicciones/'
+result_dir = '../Resultados/EROS/Tree/Regular/Predicciones/'
 
 p_dict = {}
 r_dict = {}
 f_dict = {}
+w_dict = {}
 
 for percentage in xrange(5,105,5):
+
+	# if percentage in [5,10,15,20,50,100]:
+	# 	continue
 
 	result = pd.read_csv(result_dir + 'result_' + str(percentage) + '.csv', index_col=0)
 
 	matrix = clf.confusion_matrix(result)
+
+	w_dict[percentage] = clf.weighted_f_score(matrix)
 
 	clases = matrix.columns.tolist()
 	p = [clf.precision(matrix, c) for c in clases]
@@ -30,7 +38,15 @@ for percentage in xrange(5,105,5):
 	f_dict[percentage] = f
 
 # save_dir = 'Resultados/Regular/Metricas/'
-save_dir = 'Resultados/GP/Metricas/'
+# save_dir = 'Resultados/GP/Metricas/'
+# save_dir = '../Resultados/EROS/UTree/GP/Metricas/'
+save_dir = '../Resultados/EROS/Tree/Regular/Metricas/'
+
+w_df = pd.DataFrame.from_dict(w_dict, orient='index')
+w_df.columns = ['f_score']
+w_df = w_df.sort_index(ascending=True)
+w_df = w_df.fillna(value=0.0)
+w_df.to_csv(save_dir + 'weight_fscore.csv')
 
 p_df = pd.DataFrame.from_dict(p_dict, orient='index')
 p_df.columns = clases
