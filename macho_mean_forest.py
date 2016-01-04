4625c21f658ef97a7420a2b5a68acfff9dda2bd1
Nicolas Castro <npcastrol@gmail.com>
2016-01-04 16:48:42 -0300

    Primera version de macho_mean_forest

--- macho_mean_forest.py
+++ macho_mean_forest.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 # Entreno un grupo de árboles, cada uno sobre un set de entrenamiento distinto.
-# Después al clasificar, junto la votación de cada árbol para tomar la decisión final
+# Después ocupo la media de cada curva para clasificar y junto la votación de cada árbol para tomar
+# la decisión final
 
 # -------------------------------------------------------------------------------------------------
 
@@ -21,8 +22,11 @@ if __name__ == '__main__':
         percentage = '100'
 
     folds = 10
+
     sets_path = '/n/seasfs03/IACS/TSC/ncastro/sets/MACHO_Sampled/' + percentage + '%/'
     paths = [sets_path + 'macho_sampled_' + str(i) + '.csv' for i in xrange(100)]
+
+    test_path = '/n/home09/ncastro/workspace/Features/sets/MACHO_Means/Macho means set ' + percentage + '.csv'
     
     # Para asegurar que sean las mismas curvas que en el caso normal
     # index_filter = pd.read_csv('/n/home09/ncastro/workspace/Features/sets/MACHO_Reduced/Macho reduced set '
@@ -35,7 +39,7 @@ if __name__ == '__main__':
                       'MedianBRP', 'PairSlopeTrend', 'PercentAmplitude', 'Q31', 'Rcs', 'Skew',
                       'SlottedA_length', 'SmallKurtosis', 'Std', 'StetsonK', 'StetsonK_AC']
 
-    partial_fit = partial(parallel.fit_tree, feature_filter=feature_filter, folds=10)
+    partial_fit = partial(parallel.fit_means_tree, test_path, feature_filter=feature_filter, folds=10)
     pool = Pool()
     
     resultados = pool.map(partial_fit, paths)
@@ -44,5 +48,4 @@ if __name__ == '__main__':
 
     result = metrics.aggregate_predictions(resultados)
 
-    # result.to_csv('/n/seasfs03/IACS/TSC/ncastro/Resultados/MACHO/Sampled_reduced/UF/Predicciones/result_' + percentage + '.csv')
-    result.to_csv('/n/seasfs03/IACS/TSC/ncastro/Resultados/MACHO/Sampled/UF/Predicciones/result_' + percentage + '.csv')
+    result.to_csv('/n/seasfs03/IACS/TSC/ncastro/Resultados/MACHO/Sampled/Means/Predicciones/result_' + percentage + '.csv')
--- parallel.py
+++ parallel.py
@@ -44,3 +44,64 @@ def fit_tree(path, index_filter=None, class_filter=None, feature_filter=None, fo
         
 
     return pd.concat(results)
+
+def fit_means_tree(train_path, test_path, index_filter=None, class_filter=None, feature_filter=None, folds=10):
+    """
+
+    train_path: Dirección del dataset a ocupar para entrenar
+    test_path: Dirección del dataset a ocupar para testear
+    index_filter: Pandas index para filtrar las filas del dataset que se quieren utilizar
+    class_filter: Lista de clases que se quiere utilizar
+    feature_filter: Lista de features que se quiere utilizar
+
+    """
+    train_data = pd.read_csv(train_path, index_col=0)
+    test_data = pd.read_csv(test_path, index_col=0)
+
+    if index_filter:
+        train_data = train_data.loc[index_filter]
+        test_data = test_data.loc[index_filter]
+    
+    if class_filter:
+        train_data = train_data[train_data['class'].apply(lambda x: True if x in class_filter else False)]
+        test_data = test_data[test_data['class'].apply(lambda x: True if x in class_filter else False)]
+
+    train_data = train_data.dropna(axis=0, how='any')
+    test_data = test_data.dropna(axis=0, how='any')
+
+    # Me aseguro que los datasets sean de los mismos datos
+    test_data = test_data[test_data.index.map(lambda x: x in train_data.index.tolist())]
+    train_data = train_data[train_data.index.map(lambda x: x in test_data.index.tolist())]
+    train_data = train_data.sort_index()
+    test_data = test_data.sort_index()
+
+    # Separo features de las clases
+    train_y = train_data['class']
+    train_X = train_data.drop('class', axis=1)
+
+    test_y = test_data['class']
+    test_X = test_data.drop('class', axis=1)
+
+    if feature_filter:
+        train_X = train_X[feature_filter]
+        test_X = test_X[feature_filter]
+
+    skf = cross_validation.StratifiedKFold(train_y, n_folds=folds)
+    
+    results = []
+    for train_index, test_index in skf:
+        fold_train_X = train_X.iloc[train_index]
+        fold_train_y = train_y.iloc[train_index]
+
+        fold_test_X = test_X.iloc[test_index]
+        fold_test_y = test_y.iloc[test_index]
+
+        clf = None
+        clf = tree.Tree('gain', max_depth=10, min_samples_split=20)
+
+        clf.fit(fold_train_X, fold_train_y)
+
+        result = clf.predict_table(fold_test_X, fold_test_y)
+        results.append(result)
+
+    return pd.concat(results)
