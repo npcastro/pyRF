1a7e814431cf5921f7fe99b675881aa26921fadf
Nicolas Castro <npcastrol@gmail.com>
2016-04-20 13:10:50 -0300

    Cambio big_rf para que funcione normalmente

--- Viejo/inverse/big_rf.py
+++ Viejo/inverse/big_rf.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--n_processes', required=True, type=int)
     parser.add_argument('--catalog', default='MACHO', choices=['MACHO', 'EROS', 'OGLE'])
-    parser.add_argument('--folds', required=True, type=int)
+
     parser.add_argument('--train_path',  required=True, type=str)
     parser.add_argument('--test_path',  required=True, type=str)
     parser.add_argument('--result_path',  required=True, type=str)
@@ -31,13 +31,12 @@ if __name__ == '__main__':
     parser.add_argument('--max_depth', required=False, type=int)
     parser.add_argument('--min_samples_split', required=False, type=int)
     parser.add_argument('--feature_filter',  nargs='*', type=str)
-    parser.add_argument('--index_filter', required=False, type=str)
 
     args = parser.parse_args(sys.argv[1:])
 
     n_processes = args.n_processes
     catalog = args.catalog
-    folds = args.folds
+
     train_path = args.train_path
     test_path = args.test_path
     result_path = args.result_path
@@ -46,38 +45,26 @@ if __name__ == '__main__':
     max_depth = args.max_depth
     min_samples_split = args.min_samples_split
     feature_filter = args.feature_filter
-    index_filter = args.index_filter
-
-    index_filter = pd.read_csv(index_filter, index_col=0).index
 
     train_data = pd.read_csv(train_path, index_col=0)
-    train_X, train_y = utils.filter_data(train_data, index_filter=index_filter, feature_filter=feature_filter)
+    train_index_filter = pd.read_csv('/n/seasfs03/IACS/TSC/ncastro/Resultados/MACHO/RF/Small/train.csv', index_col=0).index
+    train_X, train_y = utils.filter_data(train_data, index_filter=train_index_filter, feature_filter=feature_filter)
 
     test_data = pd.read_csv(test_path, index_col=0)
-    test_X, test_y = utils.filter_data(test_data, index_filter=index_filter, feature_filter=feature_filter)
+    test_index_filter = pd.read_csv('/n/seasfs03/IACS/TSC/ncastro/Resultados/MACHO/RF/Small/test.csv', index_col=0).index
+    test_X, test_y = utils.filter_data(test_data, index_filter=test_index_filter, feature_filter=feature_filter)
 
-    # Ocupo solo los datos de test para hacer el k-fold, por que estos no estan repetidos
-    # Y es valido ocuparlos solo por posicion
-    skf = cross_validation.StratifiedKFold(test_y, n_folds=folds)
     results = []
     ids = []
 
-    for train_index, test_index in skf:
+    clf = None
+    clf = RandomForestClassifier(n_estimators=n_estimators, criterion=criterion,
+                                 max_depth=max_depth, min_samples_split=min_samples_split,
+                                 n_jobs=n_processes)
 
-        fold_test_X = test_X.iloc[test_index]
-        fold_test_y = test_y.iloc[test_index]
-
-        fold_train_X = train_X.loc[fold_test_X.index]
-        fold_train_y = train_y.loc[fold_test_y.index]
-
-        clf = None
-        clf = RandomForestClassifier(n_estimators=n_estimators, criterion=criterion,
-                                     max_depth=max_depth, min_samples_split=min_samples_split,
-                                     n_jobs=n_processes)
-
-        clf.fit(fold_train_X, fold_train_y)
-        results.append(metrics.predict_table(clf, fold_test_X, fold_test_y))
-        ids.extend(fold_test_X.index.tolist())
+    clf.fit(train_X, train_y)
+    results.append(metrics.predict_table(clf, test_X, test_y))
+    ids.extend(test_X.index.tolist())
 
     result = pd.concat(results)
     result['indice'] = ids
